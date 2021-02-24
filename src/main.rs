aab7810c8d81696c7c548c70e396f81ade818945
Tim <tymekch@pm.me>
2021-02-25 00:58:00 +0100

    Updated server with Tera template enging

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{error, middleware, get, Error, App, web, HttpRequest, HttpResponse, HttpServer, Responder};
+use tera::Tera;
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    let mut ctx = tera::Context::new();
+    ctx.insert("name", "Test123");
+    let s = tmpl.render("index.html", &ctx).map_err(|_| error::ErrorInternalServerError("Template error"))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[get("/swap/{swapid}")]
+async fn swap(req: HttpRequest) -> impl Responder {
+    let swapid = req.match_info().get("swapid").unwrap();
+    HttpResponse::Ok().body(format!("Info about {}", swapid))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    //env_logger::init();
+
     HttpServer::new(|| {
+        let tera =
+            Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .data(tera)
+            .wrap(middleware::Logger::default()) // enable logger
+            .service(index)
+            .service(swap)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
